93351711190e2f951c630b3ac1f6e1d25b3224c6
Vadim <aonyko@gmail.com>
2024-07-30 09:02:06 +0300

    repair

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,16 +2,14 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
+
         int sum = 0;
+
         for (int i = 1; i < 7; i++) {
             sum += i;
-
-
             System.out.println(i + ") Num is " + i + ",sum is " + sum);
         }
 
-
-        System.out.println("--------------------");
-        System.out.println("Sum of number is " + sum);
+        System.out.println("--------------------\n" + "Sum of number is " + sum);
     }
 }
